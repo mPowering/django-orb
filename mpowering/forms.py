ca81d4ef0d8f24a5cc6a6babf853d1cd8006f9d2
alexlittle <alex@alexlittle.net>
2015-01-14 20:16:33 +0100

    Work on form

--- mpowering/forms.py
+++ mpowering/forms.py
@@ -6,14 +6,37 @@ from crispy_forms.bootstrap import FieldWithButtons
 from crispy_forms.layout import Button, Layout, Fieldset, ButtonHolder, Submit, Div, HTML, Row
 
 class ResourceCreateForm(forms.Form):
-    
+    title = forms.CharField(
+                required=True,
+                error_messages={'required': _('Please enter a title')},)
+    description = forms.CharField(
+                widget=forms.Textarea,
+                required=True,
+                error_messages={'required': _('Please enter a description')},)
+    image = forms.ImageField(
+                required=False,
+                error_messages={},)
+    file = forms.FileField(
+                required=False,
+                error_messages={},)
+    url = forms.CharField(
+                required=False,
+                error_messages={},)
     
     def __init__(self, *args, **kwargs):
         super(ResourceCreateForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_class = 'form-horizontal'
         self.helper.label_class = 'col-lg-2'
-        self.helper.field_class = 'col-lg-3'
+        self.helper.field_class = 'col-lg-8'
         self.helper.layout = Layout(
-                FieldWithButtons('start_date',Submit('submit', _(u'Go'), css_class='btn btn-default')),
+                'title',
+                'description',
+                'image',
+                'file',
+                'url',
+                Div(
+                   Submit('submit', _(u'Save'), css_class='btn btn-default'),
+                   css_class='col-lg-offset-2 col-lg-4',
+                ),
             )  
