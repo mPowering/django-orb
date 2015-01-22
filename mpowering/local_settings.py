dfb77acb8c644c464d9b3423d4a974a1a762b66d
alexlittle <alex@alexlittle.net>
2015-01-22 19:20:55 +0100

    CLOSED - task 14: Add html editor for description field https://github.com/alexlittle/django-mpowering/issues/issue/14

--- mpowering/local_settings.py
+++ mpowering/local_settings.py
@@ -1,6 +1,6 @@
 def modify(settings):
     
-    settings['INSTALLED_APPS'] += ('crispy_forms', 'tastypie','tinymce','django_wysiwyg',)
+    settings['INSTALLED_APPS'] += ('crispy_forms', 'tastypie', 'tinymce', 'django_wysiwyg',)
     settings['TEMPLATE_CONTEXT_PROCESSORS'] += ('mpowering.context_processors.get_menu',)
     settings['CRISPY_TEMPLATE_PACK'] = 'bootstrap3'
     settings['SHOW_GRAVATARS'] = True
