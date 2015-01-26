804de7d9d40d8025ec624e11e76061da2816bba3
alexlittle <alex@alexlittle.net>
2015-01-26 15:48:56 +0100

    OPEN - task 4: Implement search https://github.com/alexlittle/django-mpowering/issues/issue/4

--- mpowering/search_indexes.py
+++ mpowering/search_indexes.py
@@ -1,6 +1,6 @@
 import datetime
 from haystack import indexes
-from mpowering.models import Resource
+from mpowering.models import Resource, Organisation
 
 
 class ResourceIndex(indexes.SearchIndex, indexes.Indexable):
@@ -10,6 +10,19 @@ class ResourceIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_model(self):
         return Resource
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(status=Resource.APPROVED)
+    
+    
+class OrganisationIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    author = indexes.CharField(model_attr='create_user')
+    #create_date = indexes.DateTimeField(model_attr='create_date')
+
+    def get_model(self):
+        return Organisation
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
