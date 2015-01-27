d0458a326203e87b39429446a8fd01740cc2997c
alexlittle <alex@alexlittle.net>
2015-01-27 18:24:36 +0100

    Use auto_now and auto_now_add instead of timezone.now()

--- mpowering/models.py
+++ mpowering/models.py
@@ -19,17 +19,13 @@ class Organisation (models.Model):
     name = models.TextField(blank=False, null=False)
     location = models.TextField(blank=True, null=True, default=None)
     url = models.URLField(blank=True, null=True, default=None)
-    create_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(auto_now_add=True)
     create_user = models.ForeignKey(User, related_name='organisation_create_user')
-    update_date = models.DateTimeField(default=timezone.now) 
+    update_date = models.DateTimeField(auto_now=True) 
     update_user = models.ForeignKey(User, related_name='organisation_update_user')
      
     def __unicode__(self):
         return self.name
-    
-    def save(self, *args, **kwargs):
-        self.update_date = timezone.now()
-        super(Organisation, self).save(*args, **kwargs)
         
 # UserProfile
 class UserProfile (models.Model):
@@ -38,12 +34,8 @@ class UserProfile (models.Model):
     job_title = models.TextField(blank=True, null=True, default=None)
     organisation = models.ForeignKey(Organisation)
     phone_number = models.TextField(blank=True, null=True, default=None)
-    create_date = models.DateTimeField(default=timezone.now)
-    update_date = models.DateTimeField(default=timezone.now)
-    
-    def save(self, *args, **kwargs):
-        self.update_date = timezone.now()
-        super(UserProfile, self).save(*args, **kwargs)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
         
 # Resource
 class Resource (models.Model):
@@ -60,9 +52,9 @@ class Resource (models.Model):
     description = models.TextField(blank=False, null=False) 
     image = models.ImageField(upload_to='resourceimage/%Y/%m/%d', max_length=200, blank=True, null=True)
     status = models.CharField(max_length=50,choices=STATUS_TYPES)
-    create_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(auto_now_add=True)
     create_user = models.ForeignKey(User, related_name='resource_create_user')
-    update_date = models.DateTimeField(default=timezone.now) 
+    update_date = models.DateTimeField(auto_now=True) 
     update_user = models.ForeignKey(User, related_name='resource_update_user')
     slug = models.CharField(blank=True, null=True, max_length=100)
     
@@ -75,7 +67,6 @@ class Resource (models.Model):
     
     def save(self, *args, **kwargs):
         unique_slugify(self, self.title)
-        self.update_date = timezone.now()
         super(Resource, self).save(*args, **kwargs)
     
     def get_organisations(self):
@@ -115,9 +106,9 @@ class ResourceURL (models.Model):
     url = models.URLField(blank=False, null=False, max_length=500)
     resource = models.ForeignKey(Resource)
     description = models.TextField(blank=True, null=True) 
-    create_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(auto_now_add=True)
     create_user = models.ForeignKey(User, related_name='resource_url_create_user')
-    update_date = models.DateTimeField(default=timezone.now) 
+    update_date = models.DateTimeField(auto_now=True) 
     update_user = models.ForeignKey(User, related_name='resource_url_update_user')
 
     def __unicode__(self):
@@ -126,23 +117,16 @@ class ResourceURL (models.Model):
         else:
             return self.description
         
-    def save(self, *args, **kwargs):
-        self.update_date = timezone.now()
-        super(ResourceURL, self).save(*args, **kwargs)
         
 # ResourceFile
 class ResourceFile (models.Model):
     file = models.FileField(upload_to='resource/%Y/%m/%d', max_length=200)
     resource = models.ForeignKey(Resource)
     description = models.TextField(blank=True, null=True) 
-    create_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(auto_now_add=True)
     create_user = models.ForeignKey(User, related_name='resource_file_create_user')
-    update_date = models.DateTimeField(default=timezone.now) 
+    update_date = models.DateTimeField(auto_now=True) 
     update_user = models.ForeignKey(User, related_name='resource_file_update_user')
-
-    def save(self, *args, **kwargs):
-        self.update_date = timezone.now()
-        super(ResourceFile, self).save(*args, **kwargs)
     
     def filename(self):
         return os.path.basename(self.file.name)
@@ -159,15 +143,11 @@ class ResourceRelationship (models.Model):
     resource_related = models.ForeignKey(Resource, related_name='resource_related')
     relationship_type = models.CharField(max_length=50,choices=RELATIONSHIP_TYPES)
     description = models.TextField(blank=False, null=False) 
-    create_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(auto_now_add=True)
     create_user = models.ForeignKey(User, related_name='resource_relationship_create_user')
-    update_date = models.DateTimeField(default=timezone.now) 
+    update_date = models.DateTimeField(auto_now=True) 
     update_user = models.ForeignKey(User, related_name='resource_relationship_update_user')
     
-    def save(self, *args, **kwargs):
-        self.update_date = timezone.now()
-        super(ResourceRelationship, self).save(*args, **kwargs)
-        
 # Category
 class Category (models.Model):
     name = models.CharField(blank=False, null=False, max_length=100) 
@@ -197,9 +177,9 @@ class Category (models.Model):
 class Tag (models.Model):
     category = models.ForeignKey(Category)
     name = models.CharField(blank=False, null=False, max_length=100)
-    create_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(auto_now_add=True)
     create_user = models.ForeignKey(User, related_name='tag_create_user')
-    update_date = models.DateTimeField(default=timezone.now) 
+    update_date = models.DateTimeField(auto_now=True) 
     update_user = models.ForeignKey(User, related_name='tag_update_user')
     image = models.ImageField(upload_to='tag', null=True, blank=True)
     slug = models.CharField(blank=True, null=True, max_length=100)
@@ -214,21 +194,20 @@ class Tag (models.Model):
     
     def save(self, *args, **kwargs):
         unique_slugify(self, self.name)
-        self.update_date = timezone.now()
         super(Tag, self).save(*args, **kwargs)
         
 # ResourceTag
 class ResourceTag (models.Model):
     resource = models.ForeignKey(Resource)
     tag = models.ForeignKey(Tag)
-    create_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(auto_now_add=True)
     create_user = models.ForeignKey(User, related_name='resourcetag_create_user')   
 
 # ResourceOrganisation
 class ResourceOrganisation (models.Model):
     resource = models.ForeignKey(Resource)
     organisation = models.ForeignKey(Organisation)
-    create_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(auto_now_add=True)
     create_user = models.ForeignKey(User)  
     
 class ResourceTracker(models.Model):
@@ -247,7 +226,7 @@ class ResourceTracker(models.Model):
     resource = models.ForeignKey(Resource, blank=True, null=True, default=None)
     resource_file = models.ForeignKey(ResourceFile, blank=True, null=True, default=None)
     resource_url = models.ForeignKey(ResourceURL, blank=True, null=True, default=None)
-    access_date = models.DateTimeField(default=timezone.now)
+    access_date = models.DateTimeField(auto_now_add=True)
     ip = models.IPAddressField(blank=True, null=True, default=None)
     user_agent = models.TextField(blank=True, null=True, default=None)
     extra_data = models.TextField(blank=True, null=True, default=None)
@@ -256,7 +235,7 @@ class SearchTracker(models.Model):
     user = models.ForeignKey(User, blank=True, null=True, default=None)
     query = models.TextField(blank=True, null=True, default=None)
     no_results = models.IntegerField(blank=True, null=True, default=0)
-    access_date = models.DateTimeField(default=timezone.now)
+    access_date = models.DateTimeField(auto_now_add=True)
     ip = models.IPAddressField(blank=True, null=True, default=None)
     user_agent = models.TextField(blank=True, null=True, default=None)
     
