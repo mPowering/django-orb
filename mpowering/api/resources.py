07f54bd84c520475c5d181db502493b162d39566
alexlittle <alex@alexlittle.net>
2015-02-03 19:09:18 +0100

    OPEN - task 34: Add API for getting resources https://github.com/alexlittle/django-mpowering/issues/issue/34

--- mpowering/api/resources.py
+++ mpowering/api/resources.py
@@ -1,4 +1,7 @@
 
+from django.conf import settings
+from django.core.urlresolvers import reverse
+
 from tastypie import fields
 from tastypie.authentication import Authentication,ApiKeyAuthentication
 from tastypie.authorization import ReadOnlyAuthorization
@@ -6,11 +9,13 @@ from tastypie.models import ApiKey
 from tastypie.resources import ModelResource
 
 from mpowering.api.serializers import PrettyJSONSerializer, ResourceSerializer
-from mpowering.models import Resource, ResourceOrganisation, Organisation
+from mpowering.models import Resource, ResourceOrganisation, Organisation, ResourceFile, ResourceURL, ResourceTag, Tag, Category
 
 class ResourceResource(ModelResource):
     organisations = fields.ToManyField('mpowering.api.resources.ResourceOrganisationResource', 'resourceorganisation_set', related_name='resource', full=True)
-    
+    files = fields.ToManyField('mpowering.api.resources.ResourceFileResource', 'resourcefile_set', related_name='resource', full=True)
+    urls = fields.ToManyField('mpowering.api.resources.ResourceURLResource', 'resourceurl_set', related_name='resource', full=True)
+    tags = fields.ToManyField('mpowering.api.resources.ResourceTagResource', 'resourcetag_set', related_name='resource', full=True)
     class Meta:
         queryset = Resource.objects.filter(status=Resource.APPROVED)
         resource_name = 'resource'
@@ -21,6 +26,9 @@ class ResourceResource(ModelResource):
         always_return_data = True 
         include_resource_uri = True
 
+    def dehydrate_image(self,bundle):
+        return get_full_url_prefix(bundle) + settings.MEDIA_URL + bundle.obj.image.name
+    
 class ResourceOrganisationResource(ModelResource):
     organisation = fields.ToOneField('mpowering.api.resources.OrganisationResource', 'organisation', full=True)
     class Meta:
@@ -44,3 +52,73 @@ class OrganisationResource(ModelResource):
         serializer = PrettyJSONSerializer()
         always_return_data = True 
         include_resource_uri = False
+        
+class ResourceFileResource(ModelResource):
+    class Meta:
+        queryset = ResourceFile.objects.all()
+        resource_name = 'resourcefile'
+        allowed_methods = ['get']
+        fields = ['file', 'description']
+        authentication = ApiKeyAuthentication()
+        authorization = ReadOnlyAuthorization() 
+        serializer = PrettyJSONSerializer()
+        always_return_data = True 
+        include_resource_uri = False
+     
+    def dehydrate_file(self,bundle):
+        return get_full_url_prefix(bundle) + settings.MEDIA_URL + bundle.obj.file.name
+        
+class ResourceURLResource(ModelResource):
+    class Meta:
+        queryset = ResourceURL.objects.all()
+        resource_name = 'resourceurl'
+        allowed_methods = ['get']
+        fields = ['url', 'description']
+        authentication = ApiKeyAuthentication()
+        authorization = ReadOnlyAuthorization() 
+        serializer = PrettyJSONSerializer()
+        always_return_data = True 
+        include_resource_uri = False
+        
+        
+class ResourceTagResource(ModelResource):
+    tag = fields.ToOneField('mpowering.api.resources.TagResource', 'tag', full=True)
+    class Meta:
+        queryset = ResourceTag.objects.all()
+        allowed_methods = ['get']
+        include_resource_uri = False
+        authentication = ApiKeyAuthentication()
+        authorization = ReadOnlyAuthorization()
+        serializer = PrettyJSONSerializer()
+        always_return_data = True  
+        include_resource_uri = False 
+        
+        
+class TagResource(ModelResource):
+    url = fields.CharField(readonly=True)
+    class Meta:
+        queryset = Tag.objects.all()
+        resource_name = 'tag'
+        allowed_methods = ['get']
+        fields = ['name', 'image']
+        authentication = ApiKeyAuthentication()
+        authorization = ReadOnlyAuthorization() 
+        serializer = PrettyJSONSerializer()
+        always_return_data = True 
+        include_resource_uri = False
+        
+    def dehydrate_url(self,bundle):
+        url = get_full_url_prefix(bundle) + reverse('mpowering_tags', args=[bundle.obj.slug])
+        return url
+    
+def get_full_url_prefix(bundle):
+    if bundle.request.is_secure():
+        prefix = 'https://'
+    else:
+        prefix = 'http://'
+    if bundle.request.META['SERVER_PORT'] != 80 and bundle.request.META['SERVER_PORT'] != 443:
+        port =  ":" + bundle.request.META['SERVER_PORT']
+    else:
+        port = ""
+    return prefix + bundle.request.META['SERVER_NAME'] + port
+    
--- mpowering/api/serializers.py
+++ mpowering/api/serializers.py
@@ -14,10 +14,38 @@ class PrettyJSONSerializer(Serializer):
         
         
 class ResourceSerializer(Serializer):
-    json_indent = 4
+    json_indent = 2
 
     def to_json(self, data, options=None):
         options = options or {}
         data = self.to_simple(data, options)
+        
+        if 'objects' in data:
+            for o in data['objects']:
+                self.format_resource(o)
+        else:
+            self.format_resource(data)
+            
         return json.dumps(data,
                 sort_keys=True, ensure_ascii=False, indent=self.json_indent)
+
+    def format_resource(self, data):
+        # refactor tags
+        
+        for tag in data['tags']:
+            del tag['id']
+            del tag['create_date']
+            for qkey, qvalue in tag['tag'].items():
+                if qvalue is not None:
+                    tag[qkey] = qvalue
+            del tag['tag']
+            
+        # refactor organisations
+        for org in data['organisations']:
+            del org['id']
+            del org['create_date']
+            for qkey, qvalue in org['organisation'].items():
+                if qvalue is not None:
+                    org[qkey] = qvalue
+            del org['organisation']
+        return data  
