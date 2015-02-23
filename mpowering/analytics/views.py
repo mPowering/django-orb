f967bbfdb79417cf8cb02c7f86691eed2a3089f9
alexlittle <alex@alexlittle.net>
2015-02-23 14:24:25 +0100

    OPEN - task 49: Admin pages for monitoring http://github.com/mPowering/django-mpowering/issues/issue/49

--- mpowering/analytics/views.py
+++ mpowering/analytics/views.py
@@ -1,18 +1,23 @@
 
+import datetime
+from django.db.models import Count
 from django.shortcuts import render,render_to_response
 from django.template import RequestContext
+from django.utils import timezone
 from django.utils.translation import ugettext as _
 
 from mpowering.analytics.models import UserLocationVisualization
 
+from mpowering.models import SearchTracker
 
 # Create your views here.
 
 
 def home_view(request):
-    
+    start_date = timezone.now() - datetime.timedelta(days=31)
+    popular_searches = SearchTracker.objects.filter(access_date__gte=start_date).values('query').annotate(total_hits=Count('id')).order_by('-total_hits')[:10]
     return render_to_response('mpowering/analytics/home.html',
-                              {},
+                              {'popular_searches': popular_searches },
                               context_instance=RequestContext(request))
     
 def map_view(request):
