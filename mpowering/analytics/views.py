fd440fb949e3334d531d8be3c28a00aee012aa1c
alexlittle <alex@alexlittle.net>
2015-03-06 12:47:37 +0100

    OPEN - task 113: Provide analytics for content provider organisations http://github.com/mPowering/django-mpowering/issues/issue/113

--- mpowering/analytics/views.py
+++ mpowering/analytics/views.py
@@ -1,5 +1,7 @@
 
 import datetime
+
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.db.models import Count
 from django.http import HttpResponse
 from django.shortcuts import render,render_to_response
@@ -9,7 +11,7 @@ from django.utils.translation import ugettext as _
 
 from mpowering.analytics.models import UserLocationVisualization
 
-from mpowering.models import SearchTracker, ResourceTracker, Tag
+from mpowering.models import Resource, SearchTracker, ResourceTracker, Tag
 
 # Create your views here.
 
@@ -41,12 +43,25 @@ def org_view(request, id):
         return HttpResponse(status=401,content="Not Authorized") 
     
     organisation = Tag.objects.get(pk=id, category__slug='organisation')
-    print organisation.name
+    start_date = timezone.now() - datetime.timedelta(days=31)
+    trackers = ResourceTracker.objects.filter(access_date__gte=start_date,resource__resourcetag__tag=organisation,resource__status=Resource.APPROVED).order_by('-access_date')
+    
+    paginator = Paginator(trackers, 20)
+    # Make sure page request is an int. If not, deliver first page.
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+    
+    try:
+        trackers = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        trackers = paginator.page(paginator.num_pages)
     
     return render_to_response('mpowering/analytics/organisation.html',
                               {
                                'organisation': organisation,
-                               },
+                               'page':trackers,},
                               context_instance=RequestContext(request))
     
 # scan url
