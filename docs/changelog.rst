c60e5f8d869a768ee76c18f7a5f2dcc1489a293b
alexlittle <alex@alexlittle.net>
2015-03-15 14:17:05 +0100

    CLOSED - task 118: Add write API for resources - alpha version at least http://github.com/mPowering/django-mpowering/issues/issue/118

--- docs/changelog.rst
+++ docs/changelog.rst
@@ -1,6 +1,6 @@
 Change log
 =====================================
 
-
+* 118: Add write API for resources - alpha version at least http://github.com/mPowering/django-mpowering/issues/issue/118
 * 109: Add option of hierarchy of tags http://github.com/mPowering/django-mpowering/issues/issue/109
 * 26: How to add the actual files to the search index http://github.com/mPowering/django-mpowering/issues/issue/26
--- mpowering/api/upload.py
+++ mpowering/api/upload.py
@@ -5,11 +5,13 @@ from django.contrib.auth import authenticate
 from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.shortcuts import render,render_to_response
 from django.template import RequestContext
+from django.utils.translation import ugettext as _
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib import messages
+
+from mpowering.models import Resource, ResourceFile
 
 from tastypie.authentication import ApiKeyAuthentication
-
+from tastypie.exceptions import BadRequest
 
 @csrf_exempt
 def image_view(request):
@@ -17,40 +19,71 @@ def image_view(request):
     if request.method != 'POST':
         return HttpResponse(status=405)
     
-    required = ['tags','is_draft']
-   
     auth = ApiKeyAuthentication()
     auth_result = auth.is_authenticated(request)
-    print auth.user()
-    print auth_result
-    #request.headers['Authorization'] = 'ApiKey %s:%s' % (username, api_key)
-    #print username
-    return HttpResponse(status=201)
+    if auth_result == False:
+        return HttpResponse(status=401)
+    elif auth_result != True:
+        return auth_result
+
+    required_params = ['resource_id']
     
-    if not request.POST['username'] or not request.POST['api_key']:
-        #return self._unauthorized()
-        try:
-            user = User.objects.get(username=request.POST['username'])
-        except (User.DoesNotExist, User.MultipleObjectsReturned):
-            #return self._unauthorized()
-            pass
-        
-    for r in required:
+    for r in required_params:
         if r not in request.POST:
-            print r + " not found"
-            return HttpResponse(status=400)
+            return HttpResponse(status=400, content='{ "error": "No '+r+' provided"}')
+     
+    if 'image_file' not in request.FILES:
+       return HttpResponse(status=400, content='{ "error": "No image file provided"}')
+      
+    # check owner of resource
+    resource_id = request.POST['resource_id']
+    try:
+        resource = Resource.objects.get(create_user=request.user,pk=resource_id)
+    except Resource.DoesNotExist:
+        return HttpResponse(status=401)
+    
+    # handle file upload
+    resource.image = request.FILES['image_file']
+    resource.save()
    
-    
-    if 'course_file' not in request.FILES:
-        print "Course file not found"
-        return HttpResponse(status=400)
-    
-    return HttpResponse(status=201)
+    return HttpResponse(status=200)
 
 
 @csrf_exempt
 def file_view(request):
     if request.method != 'POST':
         return HttpResponse(status=405)
+    
+    auth = ApiKeyAuthentication()
+    auth_result = auth.is_authenticated(request)
+    if auth_result == False:
+        return HttpResponse(status=401)
+    elif auth_result != True:
+        return auth_result
+
+    required_params = ['resource_id','title', 'description']
+    
+    for r in required_params:
+        if r not in request.POST:
+            return HttpResponse(status=400, content='{ "error": "No '+r+' provided"}')
+     
+    if 'resource_file' not in request.FILES:
+       return HttpResponse(status=400, content='{ "error": "No resource file provided"}')
+      
+    # check owner of resource
+    resource_id = request.POST['resource_id']
+    try:
+        resource = Resource.objects.get(create_user=request.user,pk=resource_id)
+    except Resource.DoesNotExist:
+        return HttpResponse(status=401)
+    
+    rf = ResourceFile()
+    rf.title = request.POST['title']
+    rf.resource = resource
+    rf.create_user = request.user
+    rf.update_user = request.user
+    rf.file = request.FILES['resource_file']
+    rf.description = request.POST['description']
+    rf.save()
     
     return HttpResponse(status=201)
