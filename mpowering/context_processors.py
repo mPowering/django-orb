25f0e34d44ef149cf00ddfb00f5b649c49e7cc4b
alexlittle <alex@alexlittle.net>
2015-03-20 21:48:14 +0300

    OPEN - task 113: Provide analytics for content provider organisations http://github.com/mPowering/django-mpowering/issues/issue/113

--- mpowering/context_processors.py
+++ mpowering/context_processors.py
@@ -1,10 +1,12 @@
 # mpowering/context_processors.py
 from django.conf import settings
-from mpowering.models import Category, Tag
+from mpowering.models import Category, Tag, TagOwner
 
 def get_menu(request):
     categories = Category.objects.filter(top_level=True).order_by('order_by') 
     for c in categories:
         c.tags = Tag.objects.filter(category=c).order_by('order_by') 
-       
-    return {'header_menu_categories': categories, }
+    
+    tags = TagOwner.objects.filter(user=request.user)   
+    return {'header_menu_categories': categories, 
+            'header_owns_tags': tags,}
