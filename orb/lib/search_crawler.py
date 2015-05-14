36ad77fcdd0375e1e98ca87adc6b070a18e9be94
alexlittle <alex@alexlittle.net>
2015-05-14 09:54:31 -0400

    add search crawler

--- orb/lib/search_crawler.py
+++ orb/lib/search_crawler.py
@@ -1,7 +1,20 @@
 
 
 def is_search_crawler(user_agent):
-    spiders = {'Googlebot', 'Yammybot', 'Openbot', 'Yahoo', 'Slurp', 'msnbot', 'ia_archiver', 'Lycos', 'Scooter', 'AltaVista', 'Teoma', 'Gigabot', 'Googlebot-Mobile'}
+    spiders = {'Googlebot', 
+               'Yammybot', 
+               'Openbot', 
+               'Yahoo', 
+               'Slurp', 
+               'msnbot', 
+               'ia_archiver', 
+               'Lycos', 
+               'Scooter', 
+               'AltaVista', 
+               'Teoma', 
+               'Gigabot', 
+               'Googlebot-Mobile',
+               'MJ12bot'}
     for s in spiders:
         if s in user_agent:
             return True
