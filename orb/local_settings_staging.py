925de418121dfe9a592658c3dc68a007cea84f87
alexlittle <alex@alexlittle.net>
2015-05-25 09:50:31 -0400

    OPEN - task 275: Copy local_settings.py for dev, staging and live http://github.com/mPowering/django-orb/issues/issue/275

---
+++ orb/local_settings_staging.py
@@ -0,0 +1,41 @@
+from django.core.urlresolvers import reverse_lazy
+
+def modify(settings):
+    
+    settings['INSTALLED_APPS'] += ('crispy_forms', 'tastypie', 'tinymce', 'django_wysiwyg', 'haystack', 'sorl.thumbnail', 'orb.analytics')
+    settings['MIDDLEWARE_CLASSES'] += ('orb.middleware.SearchFormMiddleware',)
+    settings['TEMPLATE_CONTEXT_PROCESSORS'] += ('orb.context_processors.get_menu',)
+    settings['TEMPLATE_CONTEXT_PROCESSORS'] += ('orb.context_processors.get_version',)
+    settings['CRISPY_TEMPLATE_PACK'] = 'bootstrap3'
+    settings['LOGIN_URL'] = reverse_lazy('profile_login')
+    
+    settings['SHOW_GRAVATARS'] = True
+    settings['ORB_GOOGLE_ANALYTICS_CODE'] = 'UA-58593028-1'
+    
+    settings['ORB_INFO_EMAIL'] = 'info@mpoweringhealth.org'
+    
+    settings['ORB_RESOURCE_DESCRIPTION_MAX_WORDS'] = 150
+    
+    settings['TASK_UPLOAD_FILE_TYPE_BLACKLIST'] = [u'application/vnd.android']
+    
+    settings['TASK_UPLOAD_FILE_MAX_SIZE'] = "5242880"
+    
+    settings['DJANGO_WYSIWYG_FLAVOR'] = "tinymce_advanced"
+    
+    settings['HAYSTACK_CONNECTIONS'] = {
+        'default': {
+           'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
+        }
+    }                                  
+    settings['HAYSTACK_SIGNAL_PROCESSOR'] = 'haystack.signals.RealtimeSignalProcessor'
+    
+    settings['ADVANCED_SEARCH_CATEGORIES'] = [('health_topic','health-domain' ),
+                                         ('resource_type','type'), 
+                                         ('audience', 'audience'), 
+                                         ('geography', 'geography'), 
+                                         ('language', 'language'), 
+                                         ('device', 'device'), 
+                                         ('license', 'license')]
+    
+
+
