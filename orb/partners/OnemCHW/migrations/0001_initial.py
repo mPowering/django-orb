8c77a087e77a59922773580ffef12f75eb8b186d
alexlittle <alex@alexlittle.net>
2015-10-27 17:11:54 -0400

    update 1mCHW migrations

--- orb/partners/OnemCHW/migrations/0001_initial.py
+++ orb/partners/OnemCHW/migrations/0001_initial.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
 
 
 class Migration(migrations.Migration):
@@ -23,6 +25,9 @@ class Migration(migrations.Migration):
                 ('no_childbearing_age', models.BigIntegerField(null=True, blank=True)),
                 ('fertility_rate', models.DecimalField(null=True, max_digits=6, decimal_places=2, blank=True)),
                 ('under5_mortality', models.IntegerField(null=True, blank=True)),
+                ('last_census_year', models.IntegerField(null=True, blank=True)),
+                ('create_date', models.DateTimeField(default=datetime.datetime(2015, 10, 27, 21, 9, 13, 761625, tzinfo=utc), auto_now_add=True)),
+                ('update_date', models.DateTimeField(default=datetime.datetime(2015, 10, 27, 21, 9, 13, 761666, tzinfo=utc), auto_now=True)),
             ],
             options={
                 'ordering': ('country_name',),
