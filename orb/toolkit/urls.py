02137ef2994692d19af030fd783464d4ec35e2a7
alexlittle <alex@alexlittle.net>
2015-11-02 13:34:24 -0500

    342: Add area for toolkits
    Task-Url: http://github.com/mPowering/django-orb/issues/issues/issue/342

---
+++ orb/toolkit/urls.py
@@ -0,0 +1,12 @@
+# orb/urls.py
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
+
+urlpatterns = patterns('',
+    
+    
+    url(r'^$', 'orb.toolkit.views.toolkit_home_view', name="orb_toolkit_home"),
+    
+)
