cf513e78149869d81b1f8feedf161d78edcaf2c1
alexlittle <alex@alexlittle.net>
2016-01-14 10:49:30 +0200

    add project export scripts

---
+++ orb/utils/export-scripts/maf_export.py
@@ -0,0 +1,34 @@
+
+import tablib
+
+
+def run():
+    
+    from orb.models import Tag, Resource
+    
+    tag = Tag.objects.get(name='Medical Aid Films')
+    resources = Resource.objects.filter(resourcetag__tag=tag, status=Resource.APPROVED).order_by('title')
+    print resources
+    
+    headers = ('Resource Title', 'Tags')
+    data = []
+    data = tablib.Dataset(*data, headers=headers)
+    
+    
+    for resource in resources:
+        
+        tags = Tag.objects.filter(resourcetag__resource=resource).values_list('name', flat=True)
+        data.append(
+                    (
+                        resource.title, 
+                        ', '.join(tags) 
+                    )
+                )
+
+    with open( "/home/alex/temp/maf.xls", 'wb') as f:
+        f.write(data.xls)  
+
+if __name__ == "__main__":
+    import django
+    django.setup()
+    run()  
