c855668e0431ebac3ccae8f221667629e45eda17
Ben Lopatin <ben@benlopatin.com>
2016-03-23 13:36:29 -0400

    Set up dev project management and consolidate tests
    - Consolidate tests into tests module
    - Add comprehensive base gitignore
    - Formatting on setup.py
    - Add configuration to run & test app in place. Obviates the need for a
      container project for tests and devserver
    - Break out tests further by logical module
    
    Subsequent tests can be organized by which logical group they
    support, where "logical groups" represent what should be individual
    Django apps. E.g. "orb.tags" for tags.

--- README.txt
+++
@@ -1,7 +0,0 @@
-===========================
-ORB Content Platform
-===========================
-
-For documentation visit: http://mpowering.readthedocs.org
-
-For information on mPowering visit: http://mpoweringhealth.org
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -9,15 +9,17 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-orb',
     version='2.1.0',
-    packages=['orb',
-              'orb.analytics',
-              'orb.api',
-              'orb.migrations', 
-              'orb.fixtures',
-              'orb.utils',
-              'orb.profile',],
+    packages=[
+        'orb',
+        'orb.analytics',
+        'orb.api',
+        'orb.migrations',
+        'orb.fixtures',
+        'orb.utils',
+        'orb.profile',
+    ],
     include_package_data=True,
-    license='GNU GPL v3 License',  # example license
+    license='GNU GPL v3 License',
     description='',
     long_description=README,
     url='http://oppia-mobile.org/',
@@ -31,23 +33,22 @@ setup(
         'Development Status :: 4 - Beta',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     install_requires=[
-        "Django >= 1.7",
-	    "django-tastypie >= 0.11.0",
-        "django-tablib >= 0.9.11",
-        "django-crispy-forms >= 1.4.0",
-        "django-tinymce",
-        "django-wysiwyg",
-        "django-haystack",
-        "pysolr",
-        "pillow",
-        "sorl-thumbnail",
-        "textract >= 1.2.0",
-        "pytz",
+        "Django>=1.7,<1.8",
+        "django-tastypie>=0.11.0,<1.13.0",
+        "django-tablib>=0.9.11,<4.0",
+        "django-crispy-forms>=1.4.0,<2.0",
+        "django-tinymce>=1.5.0,<2.0",
+        "django-wysiwyg>=0.7.0,<1.0",
+        "django-haystack>=2.3.0,<2.4.0",
+        "pysolr>=3.3.0,<3.4.0",
+        "Pillow>=2.7.0,<3.0.0",
+        "sorl-thumbnail>=12.0,<13.0",
+        "textract>=1.2.0,<1.3.0",
+        "pytz>=2014,<2015",
     ],
 )
