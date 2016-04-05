eb44f57c728a790f5b7a3f085bb5bb4a9b10d7f8
Ben Lopatin <ben@benlopatin.com>
2016-04-05 11:21:28 -0400

    Add ISO 639 languages fixtures and import command
    ISO 639 languages are included in a CSV file for convenient reading and
    editing. Data source is from the US Library of Congress
    (http://loc.gov/standards/iso639-2/php/code_list.php). French and German
    names are included for completeness.
    
    The management command loads the fixture file (or a different file) and
    runs through the list ensuring that it does not duplicate any tags. Each
    tag is categorized and assigned a creator and updater, which can be
    overridden in the command arguments.
    
    By default this only loads lanugages with ISO 639-1 codes, excluding
    many languages. The complete list includes many listing which should
    probably _not_ be included (e.g. Klingon), and without a strategy for
    excluding these the command and data combination should be more
    conservative.
    
    Closes gh-232
    [Fixes #116086691]

---
+++ orb/management/commands/load_orb_languages.py
@@ -0,0 +1,80 @@
+"""
+Management command to load language fixtures as tags
+"""
+import os
+import csv
+import re
+from optparse import make_option
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+from orb.models import Category, Tag
+
+
+def has_data(input):
+    """Identify if the input contains any meaningful string content
+
+    CSV input may include non-breaking space which is a Unicode character,
+    however the csv module does not handle unicode.
+
+    Args:
+        input: string value
+
+    Returns:
+        bool
+
+    """
+    input = input.replace("\xc2\xa0", " ")
+    return bool(re.compile("\S").match(input))
+
+
+class Command(BaseCommand):
+    help = "Loads languages from CSV fixtures into tag database"
+
+    option_list = BaseCommand.option_list + (
+        make_option("--file",
+            dest="fixture",
+            default="orb/fixtures/iso639.csv",
+            help="CSV file path"),
+        make_option("--image",
+            dest="image",
+            default="tag/language_default.png",
+            help="Default image (static image path)"),
+        make_option("--user",
+            dest="user",
+            type="int",
+            default=1,
+            help="Default user to mark as creating"),
+        make_option("--iso6392",
+            action="store_true",
+            dest="iso6392",
+            default=False,
+            help="Flag for including all ISO 639.2 (only ISO 639.1 included by default)"),
+        )
+
+    def handle(self, *args, **options):
+
+        try:
+            user = User.objects.get(pk=options["user"])
+        except User.DoesNotExist:
+            raise CommandError("No match user found for '{0}'".format(options["user"]))
+
+        category = Category.objects.filter(name="Language").first()
+
+        if not os.path.exists(options["fixture"]):
+            raise CommandError("Cannot find file '{0}'".format(options["fixture"]))
+
+        with open(options["fixture"]) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+
+                if not options["iso6392"] and not has_data(row["iso639-1"]):
+                    continue
+
+                tag, _ = Tag.objects.get_or_create(name=row["English"], defaults={
+                    "create_user": user,
+                    "update_user": user,
+                    "category": category,
+                    "image": options["image"],
+                })
+
