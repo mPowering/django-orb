36220edb9e60b0248cd743ca0cfcc68d09e57915
alexlittle <alex@digital-campus.org>
2016-05-03 16:58:41 +0300

    344: Country map - update to show content we have for latin/south america
    Task-Url: http://github.com/mPowering/django-orb/issues/issues/issue/344

--- orb/__init__.py
+++ orb/__init__.py
@@ -1,1 +1,1 @@
-VERSION = (2, 1, 1, 'beta', 0)
+VERSION = (2, 2, 1, 'beta', 0)
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-orb',
-    version='2.1.1',
+    version='2.2.1',
     packages=[
         'orb',
         'orb.analytics',
