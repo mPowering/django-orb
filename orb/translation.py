d492304ba3c9eb85777f90398d929da2b74e77e9
Ben Lopatin <ben@benlopatin.com>
2016-05-16 19:05:27 -0400

    Rename translation file and fix registration
    registration as decorator is only available in newer versions of
    modeltranslation
    
    [#118828819]

--- orb/translation.py
+++ orb/translation.py
@@ -7,13 +7,13 @@ from modeltranslation.translator import translator, TranslationOptions
 from .models import Category, Tag
 
 
-@translator.register(Category)
 class CategoryTranslation(TranslationOptions):
     fields = ('name',)
 
 
-@translator.register(Tag)
 class TagTranslation(TranslationOptions):
     fields = ('name', 'description', 'summary')
 
 
+translator.register(Category, CategoryTranslation)
+translator.register(Tag, TagTranslation)
