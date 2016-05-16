b2f858e260e2d6177fb112baf948b53248f791b5
Ben Lopatin <ben@benlopatin.com>
2016-05-16 14:55:47 -0400

    Add missing LocaleMiddleware
    [#118828107]

--- config/settings.py
+++ config/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 MIDDLEWARE_CLASSES = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
