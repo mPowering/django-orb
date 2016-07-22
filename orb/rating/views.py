156070984c1e9ca3c7ce29bba7865c51b74e88f8
Ben Lopatin <ben@benlopatin.com>
2016-07-22 11:52:25 -0400

    Stash changes

--- orb/rating/views.py
+++ orb/rating/views.py
@@ -3,49 +3,42 @@ import json
 
 from django.conf import settings
 from django.http import Http404, HttpResponse, HttpResponseBadRequest
+from django.views.decorators.http import require_POST
 
 from orb.models import Resource, ResourceRating
 
 
+@require_POST
 def resource_rate_view(request):
     if request.user.is_anonymous():
         raise Http404()
-    if request.method == 'POST':
-        resource_id = request.POST.get('resource_id')
-        rating = request.POST.get('rating')
-        #comment = request.POST.get('comment')
 
-        if resource_id is None or rating is None:
-            return HttpResponseBadRequest()
+    resource_id = request.POST.get('resource_id')
+    rating = request.POST.get('rating')
 
-        rating = int(rating)
-        if rating > 5 or rating < 1:
-            return HttpResponseBadRequest()
-
-        try:
-            resource = Resource.objects.get(pk=resource_id)
-        except Resource.DoesNotExist:
-            return HttpResponseBadRequest()
-
-        # TODO replace with get_or_create
-        # check if an update or new rating
-        try:
-            user_rated = ResourceRating.objects.get(
-                resource=resource, user=request.user)
-            user_rated.rating = rating
-            user_rated.save()
-        except ResourceRating.DoesNotExist:
-            rating_obj = ResourceRating()
-            rating_obj.resource = resource
-            rating_obj.rating = rating
-            rating_obj.user = request.user
-            rating_obj.save()
-
-        # TODO consolidate as one dict initialization
-        resp_obj = {}
-        resp_obj['no_ratings'] = ResourceRating.objects.filter(
-            resource=resource).count()
-        resp_obj['ratings_required'] = settings.ORB_RESOURCE_MIN_RATINGS
-        return HttpResponse(json.dumps(resp_obj), content_type="application/json; charset=utf-8")
-    else:
+    if resource_id is None or rating is None:
         return HttpResponseBadRequest()
+
+    rating = int(rating)
+    if rating > 5 or rating < 1:
+        return HttpResponseBadRequest()
+
+    try:
+        resource = Resource.objects.get(pk=resource_id)
+    except Resource.DoesNotExist:
+        return HttpResponseBadRequest()
+
+    user_rating, created = ResourceRating.objects.get_or_create(
+        resource=resource,
+        user=request.user,
+        defaults={'rating': rating},
+    )
+    if not created:
+        user_rating.rating = rating
+        user_rating.save()
+
+    resp_obj = {
+        'no_ratings': ResourceRating.objects.filter(resource=resource).count(),
+        'ratings_required': settings.ORB_RESOURCE_MIN_RATINGS,
+    }
+    return HttpResponse(json.dumps(resp_obj), content_type="application/json; charset=utf-8")
