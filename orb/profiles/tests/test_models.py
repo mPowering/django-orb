843936c57a6f196678da2a38499d001535bb1dbd
Ben Lopatin <ben@benlopatin.com>
2016-08-14 19:09:52 -0400

    Update tests for updated reviewer management

--- orb/profiles/tests/test_models.py
+++ orb/profiles/tests/test_models.py
@@ -1,12 +1,18 @@
 from django.test import TestCase
-from orb.models import UserProfile
+import mock
+from orb.models import UserProfile, ReviewerRole
 
 
 class ProfileMethodTests(TestCase):
     """Tests on class methods"""
 
     def test_reviewer(self):
-        self.assertTrue(UserProfile(crt_member=True).is_reviewer)
-        self.assertTrue(UserProfile(mep_member=True).is_reviewer)
-        self.assertTrue(UserProfile(mep_member=True, crt_member=True).is_reviewer)
+        model_state = mock.Mock()
+        model_state.db = 'default'
+        mocked_role = mock.Mock(spec=ReviewerRole)
+        mocked_role._state = model_state
+        profile = UserProfile(reviewer_role=mocked_role)
+
+        self.assertTrue(profile.is_reviewer)
+
         self.assertFalse(UserProfile().is_reviewer)
