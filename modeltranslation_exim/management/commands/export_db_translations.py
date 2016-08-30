f51a19ea86cd64026e139fe39a8c70a994f75399
Ben Lopatin <ben@benlopatin.com>
2016-08-30 11:50:26 -0400

    Add scaffolding for export management command

---
+++ modeltranslation_exim/management/commands/export_db_translations.py
@@ -0,0 +1,7 @@
+"""
+Management command that generates a PO file for a specified language
+based on django-modeltranslation fields.
+
+This generates a quasi-complete PO file, in that it cannot be turned
+into a usable compiled MO file.
+"""
