585344e5a4ea136a634c7f946ca3a84978d6c3f1
Ben Lopatin <ben@benlopatin.com>
2016-12-08 07:38:34 -0500

    Add list_peers command

---
+++ orb/peers/management/commands/list_peers.py
@@ -0,0 +1,35 @@
+"""
+Management command to list ORB peer data
+
+Usage:
+
+    $ django-admin.py list_peers
+
+      [1] Malawi ORB, https://www.malawi-orb.org
+      [2] ORB Pakistan, https://www.orb.pk
+
+"""
+
+from django.core.management.base import BaseCommand
+
+from orb.peers.models import Peer
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        active = Peer.peers.active()
+        inactive = Peer.peers.inactive()
+
+        if not active or inactive:
+            print("No peers have been registered.")
+
+        if active:
+            print("\nActive peers\n")
+            for peer in active:
+                print("[{}] {}, {}".format(peer.pk, peer.name, peer.host))
+
+        if inactive:
+            print("\nInctive peers (unsynced)\n")
+            for peer in inactive:
+                print("[{}] {}, {}".format(peer.pk, peer.name, peer.host))
