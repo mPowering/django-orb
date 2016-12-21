8e7acb641dc7f5e698f2859b8ff1c11af24a53bd
Ben Lopatin <ben@benlopatin.com>
2016-12-21 07:33:32 -0500

    Add remote resource and import user test fixtures

--- orb/fixtures/__init__.py
+++ orb/fixtures/__init__.py
@@ -19,6 +19,13 @@ def test_user():
 
 
 @pytest.fixture(scope="session")
+def import_user():
+    user, _ = User.objects.get_or_create(username="importer")
+    yield user
+
+
+
+@pytest.fixture(scope="session")
 def test_category():
     category, _ = Category.objects.get_or_create(name="test category")
     yield category
@@ -45,4 +52,17 @@ def test_resource(test_user):
 
 @pytest.fixture(scope="session")
 def test_peer():
-    yield Peer.peers.create(name="Distant ORB", host="http://www.orb.org/")
+    peer = Peer.peers.create(name="Distant ORB", host="http://www.orb.org/")
+    yield peer
+
+
+@pytest.fixture(scope="session")
+def remote_resource(import_user, test_peer):
+    """Fixture for a remotely created resource"""
+    yield resource_factory(
+        user=import_user,
+        title=u"A remote resource",
+        description=u"<p>A remote resource</p>",
+        source_peer=test_peer,
+    )
+
