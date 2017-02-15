77ece87f4db4d9ad9bc4c4de17f2c5fb7be2a736
Ben Lopatin <ben@benlopatin.com>
2017-02-15 10:11:52 -0500

    Replace 404 error for POST, non-idiomatic response

--- orb/bookmark/views.py
+++ orb/bookmark/views.py
@@ -2,9 +2,8 @@
 
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.http import Http404, HttpResponseBadRequest, HttpResponseRedirect
+from django.http import HttpResponseBadRequest, HttpResponseRedirect
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
@@ -31,7 +30,10 @@ def resource_bookmark_view(request):
     if resource_id is None:
         return HttpResponseBadRequest()
 
-    resource = get_object_or_404(Resource, pk=resource_id)
+    try:
+        resource = Resource.objects.get(pk=resource_id)
+    except Resource.DoesNotExist:
+        return HttpResponseBadRequest()
 
     try:
         collection = Collection.objects.get(visibility=Collection.PRIVATE, collectionuser__user=request.user)
@@ -44,13 +46,15 @@ def resource_bookmark_view(request):
     return JsonResponse({'success': True})
 
 
+@login_required
 def resource_bookmark_remove_view(request, resource_id):
-    if request.user.is_anonymous():
-        raise Http404()
     # check the current user is the owner of this bookmark
     try:
         bookmark = CollectionResource.objects.get(
-            resource__pk=resource_id, collection__visibility=Collection.PRIVATE, collection__collectionuser__user=request.user)
+            resource__pk=resource_id,
+            collection__visibility=Collection.PRIVATE,
+            collection__collectionuser__user=request.user,
+        )
     except CollectionResource.DoesNotExist:
         return HttpResponseBadRequest()
 
