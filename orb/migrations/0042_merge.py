07f63a3dea438e7ba1192510f8cfaaa62d3b6dd0
Alex Little <alex@digital-campus.org>
2017-02-24 18:25:55 +0200

    fix migration conflict

---
+++ orb/migrations/0042_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('orb', '0041_auto_20170207_0051'),
+        ('orb', '0041_auto_20170220_2036'),
+    ]
+
+    operations = [
+    ]
