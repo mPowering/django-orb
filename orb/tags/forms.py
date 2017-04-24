af68eda14a58c1dbcfdf62e9287ca3d4294c99e9
Ben Lopatin <ben@benlopatin.com>
2017-04-24 12:58:53 -0400

    Properly handle request inputs as strings

--- orb/tags/forms.py
+++ orb/tags/forms.py
@@ -1,4 +1,4 @@
-
+from __future__ import unicode_literals
 from django import forms
 from django.utils.translation import ugettext as _
 
@@ -16,5 +16,19 @@ class TagPageForm(forms.Form):
         (UPDATED, _(u'Update date')),
     )
 
-    page = forms.IntegerField(min_value=1, widget=forms.HiddenInput, initial=1)
-    order = forms.ChoiceField(choices=ORDER_OPTIONS)
+    page = forms.CharField(widget=forms.HiddenInput, initial='1')
+    order = forms.ChoiceField(choices=ORDER_OPTIONS, required=False)
+
+    def clean_order(self):
+        order = self.cleaned_data.get('order', self.CREATED)
+        return order or self.CREATED
+
+    def clean_page(self):
+        page = self.cleaned_data.get('page', 1)
+        try:
+            page_value = int(page)
+        except (ValueError, TypeError):
+            raise forms.ValidationError("'{}' is not a valid page number".format(page))
+        if page_value > 0:
+            return page_value
+        raise forms.ValidationError("'{}' is not a valid page number".format(page_value))
--- orb/tags/tests/test_forms.py
+++ orb/tags/tests/test_forms.py
@@ -4,7 +4,8 @@
 Tests for ORB tag forms
 """
 
-from hypothesis import strategies as st, given, example
+from hypothesis import strategies as st, given, example, settings
+import  pytest
 
 from orb.tags.forms import TagPageForm
 
@@ -23,6 +24,30 @@ def test_tag_order_validation(order_by):
 
     else:
         assert not form.is_valid()
+
+
+@given(st.characters())
+def test_tag_page_number_validation(page):
+    form = TagPageForm(data={'page': page})
+    try:
+        page_num = int(page)
+    except (TypeError, ValueError):
+        assert not form.is_valid()
+    else:
+        if page_num > 0:
+            assert form.is_valid()
+        else:
+            assert not form.is_valid()
+
+
+@pytest.mark.parametrize("page,expected", [
+    ("1",1),
+    ("3",3),
+])
+def test_valid_page_numbers(page, expected):
+    form = TagPageForm(data={'page': page})
+    form.is_valid()
+    assert form.cleaned_data['page'] == expected
 
 
 @given(st.integers())
