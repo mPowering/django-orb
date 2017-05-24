2cacf96168f066e7448a18f3ed45277d0ae8339c
Ben Lopatin <ben@benlopatin.com>
2017-05-24 12:51:14 -0400

    Update entrypoint for Django container

--- compose/django/entrypoint.sh
+++ compose/django/entrypoint.sh
@@ -2,19 +2,6 @@
 set -e
 cmd="$@"
 
-# This entrypoint is used to play nicely with the current cookiecutter configuration.
-# Since docker-compose relies heavily on environment variables itself for configuration, we'd have to define multiple
-# environment variables just to support cookiecutter out of the box. That makes no sense, so this little entrypoint
-# does all this for us.
-
-
-# wait for mysql to be ready
-nc -z mysql 3306
-n=$?
-while [ $n -ne 0 ]; do
-    sleep 1
-    nc -z mysql 3306
-    n=$?
-done
+# ./wait-for-it.sh mysql:3306
 
 exec $cmd
