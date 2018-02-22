54a435b8333721d154dc6fbc4483ec4dd5568837
Alex Little <alex@digital-campus.org>
2018-02-22 10:50:53 +0200

    Update version no

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-orb',
-    version='2.5.3',
+    version='2.6.0',
     packages=[
         'orb',
         'orb.analytics',
