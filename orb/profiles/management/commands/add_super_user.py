6c8525485673c6b8a9d9b80afa1ded44c72b0065
Ben Lopatin <ben@benlopatin.com>
2018-03-30 12:39:41 -0400

    Add command to create super user w/ profile from CLI

---
+++ orb/profiles/management/commands/add_super_user.py
@@ -0,0 +1,51 @@
+"""
+Management command to add a superuser with CLI args including UserProfile
+
+This should only be used to programmatically create superusers with *initial*
+passwords, to be changed after creation.
+
+Usage:
+
+    django-admin.py add_super_user --email="someone@hello.com" --password="COOLPASSWORD"
+
+"""
+
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+from orb.models import UserProfile
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--email',
+            dest='email',
+            help='Email address',
+            required=True,
+        )
+        parser.add_argument(
+            '--password',
+            dest='password',
+            help='Initial password',
+            required=True,
+        )
+
+    def handle(self, *args, **options):
+
+        username = options.get('email')
+
+        try:
+            user = get_user_model().objects.get(username=username)
+        except get_user_model().DoesNotExist:
+            get_user_model().objects.create_superuser(
+                username,
+                password=options.get('password'),
+                email=options.get('email'),
+            )
+            UserProfile.objects.create(user=user)
+            self.stdout.write("Create new user '{}'".format(username))
+        else:
+            self.stdout.write("Found existing user '{}'".format(username))
+
